8fbf7713e6ab672007e2e75277ed90b2daeab5c3
anandrajB <anand98.ar@gmail.com>
2022-10-07 16:30:51 +0530

    updated

--- transaction/api/CounterPartyAPI.py
+++ transaction/api/CounterPartyAPI.py
@@ -105,7 +105,8 @@ class CounterPartyTransitionApiCoreView(APIView):
     def post(self, request, pk, *args, **kwargs):
         obj = generics.get_object_or_404(workflowitems, id=pk)
         type = self.request.query_params.get('type')
-        obj.user = self.request.user
+        bank_comments = request.data.get('bank_comments')
+        obj.user, obj.comments = self.request.user , bank_comments
         flow = CounterPartyFlow(obj)
         if type == "draft" :
             flow.Draft_flow(request)
